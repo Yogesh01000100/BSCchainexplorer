93c190b8f32dc6e51e9b7cbe665c58792fce468b
D Yogesh <90953665+Yogesh01000100@users.noreply.github.com>
2023-02-28 21:59:50 +0530

    Update BSCSCAN_RESULTS.js

--- BSCSCAN_RESULTS.js
+++ BSCSCAN_RESULTS.js
@@ -7,20 +7,26 @@ let d_status = 0;
 var ct = 5;
 let filter;
 var arr = [];
+var uniqueArray = [];
 var bal_arr = new Array();
 
-function show(arr) {
-    var unique = [];
-    arr.forEach(element => {
-        if (!unique.includes(element)) {
-            unique.push(element);
-        }
-    });
-    return unique;
+function removeDuplicates(originalArray, prop) {
+    var newArray = [];
+    var lookupObject = {};
+
+    for (var i in originalArray) {
+        lookupObject[originalArray[i][prop]] = originalArray[i];
+    }
+
+    for (i in lookupObject) {
+        newArray.push(lookupObject[i]);
+    }
+    return newArray;
 }
 //for (let w = 0; w < bal_arr.length; w++) {
 //   console.log(bal_arr[w]);
 //}
+document.getElementById("requestStatus").textContent = "NOT CONNECTED";
 
 function downloadCsv(filename, csvData) {
     const element = document.createElement("a");
@@ -41,16 +47,16 @@ let btn = document.getElementById("sendGetRequestBtn");
 let loadingel = document.getElementById("loading");
 
 
-let ele = document.getElementById("container");
+let ele = document.getElementById("ftd");
 btn.addEventListener("click", function() {
     loadingel.classList.remove("d-none");
-    const url = "https://api.bscscan.com/api?module=account&action=txlistinternal&startblock=25944043&endblock=26836255&page=1&offset=90&sort=asc&apikey=I6XJPSDTKIDNCK24XUURI65MHRFZ431U5J"
+    const url = "https://api.bscscan.com/api?module=account&action=txlistinternal&startblock=25944043&endblock=26836255&page=1&offset=500&sort=asc&apikey=I6XJPSDTKIDNCK24XUURI65MHRFZ431U5J"
     fetch(url, options)
         .then(function(response) {
             return response.status;
         })
         .then(function(status) {
-            document.getElementById("requestStatus").textContent = "Success!";
+            document.getElementById("requestStatus").textContent = "Success! You are connected to the MAINNET!";
             //console.log(status);
             if (status == 200) {
                 btn.textContent = "Connected";
@@ -69,13 +75,17 @@ btn.addEventListener("click", function() {
             const parsedResponse = JSON.parse(data);
             for (let i = 0; i < parsedResponse.result.length; i++) {
                 const firstTransaction = parsedResponse.result[i];
-                arr.push(firstTransaction.from);
+                arr.push({
+                    "address": firstTransaction.from,
+                    "timestamp": firstTransaction.timeStamp
+                });
             }
             document.getElementById("httpResponse").textContent = "Click on Fetch data button to fetch data!";
             y = 1;
             if (y == 1) {
 
-                arr = show(arr);
+                uniqueArray = removeDuplicates(arr, "address");
+                //console.log(uniqueArray[0]);
             }
             loadingel.classList.add("d-none");
         });
@@ -89,18 +99,22 @@ document.getElementById("ftd").appendChild(filter);
 filter.addEventListener("click", function() {
     document.getElementById("httpResponse").textContent = " ";
     let uniqueId = null;
-    let cent = arr.length;
+    let cent = uniqueArray.length;
+    if (cent == 0) {
+        ele.textContent = "Connect to Mainnet first! Reload page!";
+        return;
+    }
     let p = 0;
     let k = -1;
     uniqueId = setInterval(function() {
-        let add = arr[p];
+        let add = uniqueArray[p].address;
         const url = `https://api.bscscan.com/api?module=account&action=balance&address=${add}&tag=latest&apikey=I6XJPSDTKIDNCK24XUURI65MHRFZ431U5J`;
         fetch(url, options)
             .then(function(response) {
                 return response.status;
             })
             .then(function(status) {
-                document.getElementById("requestStatus").textContent = status;
+                document.getElementById("requestStatus").textContent = "IDLE";
             });
         fetch(url, options)
             .then(function(response) {
@@ -109,25 +123,28 @@ filter.addEventListener("click", function() {
             .then(function(data) {
                 const parsedResponse = JSON.parse(data);
                 //console.log(parsedResponse.result);
-                document.getElementById("httpResponse").textContent = "address: " + add + "\n" + " Balance: " + parsedResponse.result / 1000000000000000000;
+
+                document.getElementById("httpResponse").textContent = "address: " + add + "\n" + "timestamp: " + uniqueArray[p].timestamp + " Balance: " + parsedResponse.result / 1000000000000000000;
                 if ((parsedResponse.result / 1000000000000000000) > 2) {
                     bal_arr.push({
-                        address: add,
-                        balance: parsedResponse.result / 1000000000000000000
+                        "timestamp": uniqueArray[p].timestamp,
+                        "w_address": add,
+                        "balance": parsedResponse.result / 1000000000000000000
                     });
                     k = k + 1;
                 }
             });
         p = p + 1;
         if (100 * p / cent == 100) {
-            ele.textContent = "Downloaded!"
+            ele.textContent = "Click on Download button below to download data in .csv format."
         } else if (100 * p / cent == "Infinity") {
             ele.textContent = "Connect to Mainnet first! Reload page! first connect then fetch";
         } else {
-            ele.textContent = "Downloading... " + 100 * p / cent + " %";
+            ele.textContent = "Downloading... " + Number((100 * p) / cent).toFixed(2) + " %";
+            document.getElementById("requestStatus").textContent = "Fetching...";
         }
         //console.log(bal_arr[k]);
-        if (p == arr.length) {
+        if (p == uniqueArray.length) {
             clearInterval(uniqueId);
             d_status = 1;
             //console.log(d_status);
@@ -144,5 +161,5 @@ filter.addEventListener("click", function() {
             }
             return;
         }
-    }, 390);
+    }, 200);
 });
